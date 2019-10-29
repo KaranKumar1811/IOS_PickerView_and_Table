2005cf5fa053d5953cc6faf8b35d911f0515556f
Karan <Kumarkay.ca@gmail.com>
2019-10-29 18:33:29 -0400

    Adding 2 Rows in PickerView

--- IOS_PickerView_and_Table/PickerViewController.swift
+++ IOS_PickerView_and_Table/PickerViewController.swift
@@ -12,10 +12,13 @@ class PickerViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDa
     
     @IBOutlet weak var PickerUI: UIPickerView!
     
-   
+    @IBOutlet weak var CountryLabel: UILabel!
+    
     
 
     let countries = ["India","Canada","USA","China","Pakistan","Russia","Nepal","France","Mexico"]
+    let currency = ["Rupees","CAD","USD","YAN","Prupee","Euro","NRupees","euro","dollor"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.PickerUI.delegate = self
@@ -24,19 +27,40 @@ class PickerViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDa
     
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 1{
         return countries.count
+        }
+        else
+        {
+        return currency.count
+        }
     }
 
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 1
+        {
         return self.countries[row]
+        }
+        else
+        {
+        return self.currency[row]
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 1{
+        CountryLabel.text = self.countries[row]
         print(self.countries[row])
+        }
+        else{
+            print(self.currency[row])
+        }
+        
     }
 }
 
